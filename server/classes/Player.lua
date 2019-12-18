48c0bb915dbaea882e920c250452fcaeaf299d3a
final1337 <booking.finalderboss@gmail.com>
2019-12-18 20:07:57 +0100

    Fix on Player class

--- server/classes/Player.lua
+++ server/classes/Player.lua
@@ -4,6 +4,12 @@ registerElementClass("player", Player)
 
 Player.DataTable = {"Money", "Playtime", "LastLogin", "Status"}
 
+function Player:constructor()
+    self.m_CurrentLobby = false
+    self.m_LoggedIn = false
+    self.m_Data = {}
+end
+
 function Player:setData(key, value, noSync)
     self.m_Data[key] = value
     if not noSync then
@@ -32,7 +38,7 @@ end
 
 function Player:getSQLData(from, where, name, data)
     if from and where and name and data then
-        local query = dbQuery(handler, "SELECT * FROM ? WHERE ? = ?", from, where, name)
+        local query = dbQuery(handler, "SELECT * FROM "..from.." WHERE "..where.." = '"..name.."'")
         if query then
             local rows = dbPoll(query, -1)
             for k, v in pairs(rows) do
@@ -47,6 +53,6 @@ function Player:setLoginDatas()
     for k, v in ipairs(self.DataTable) do
         local data = self:getSQLData("userdata", "Name", self:getName(), v)
         self:setData(v, data)
-        print("Loaded elementdata for player "..self:getName()..": "self:getData(v))
+        print("Loaded elementdata for player "..self:getName()..": "..self:getData(v))
     end
 end
