e918142ec188ef5d3a7fb59d86b15c3feccaa9a2
final1337 <booking.finalderboss@gmail.com>
2019-12-24 00:29:47 +0100

    Added math.round useful function

---
+++ shared/mathround.lua
@@ -0,0 +1,6 @@
+function math.round(number, decimals, method)
+    decimals = decimals or 0
+    local factor = 10 ^ decimals
+    if (method == "ceil" or method == "floor") then return math[method](number * factor) / factor
+    else return tonumber(("%."..decimals.."f"):format(number)) end
+end
